5b002748b8bc9077e92f2498b9f9e9b67baa0385
RogerBV <rogerbv_9017@hotmail.com>
2019-09-12 23:38:23 -0500

    Including VueJS, Axios and Category.js

---
+++ SysLogistic/Scripts/Vue/category.js
@@ -0,0 +1,27 @@
+﻿const category = {
+    Name: ''
+    ,Description: ''
+}
+
+const eCategory = Object.create(category);
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        txtName: '',
+        txtDescription: ''
+    }, methods: {
+        SaveCategory: function (event) {
+            
+            eCategory.Name = this.txtName
+            eCategory.Description = this.txtDescription
+            const oCategory = JSON.stringify(eCategory);
+            //alert("/Category/create?newCategory='" + oCategory+"'");
+
+            axios.get("/Category/create?newCategory='" + oCategory + "'")
+                .then(response => {
+                    //this.info = response.data.bpi
+                });
+        }
+    }
+});
