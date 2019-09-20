d1e5fa869c024e5f8c0fe1b599d73a4b23be9aa2
RogerBV <rogerbv_9017@hotmail.com>
2019-09-19 20:02:47 -0500

    Product.js

---
+++ SysLogistic/Scripts/Vue/Product.js
@@ -0,0 +1,34 @@
+﻿var appProduct = new Vue({
+    el: '#appProduct',
+    data: {
+        txtName: '',
+        txtDescription: '',
+        txtPrice: 0.00,
+        cboCategory: '',
+        categories: [],
+        products: []
+    }, methods: {
+            List: function (event) {
+                axios.get('/Product/List', {
+                    dataType: 'json'
+                }).then(response => this.products = response.data.data);
+
+            },
+            ListCategories: function (event) {
+                axios.get('/Product/ListCategories', {
+                    dataType: 'json'
+                }).then(response => this.categories = response.data.data);
+            },
+        SaveProduct: function (event) {
+            axios.post("/Product/create", { Description: this.txtDescription, Name: this.txtName, Price: this.txtPrice, CategoryId: this.cboCategory })
+                        .then(response => {
+                            alert("Registro Exitoso");
+                            $('#modalProduct').modal('toggle');
+                            this.List();
+                        });
+                }
+    }, mounted() {
+        this.List();
+        this.ListCategories();
+    }
+});
